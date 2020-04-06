6493409e85c385fef175ef22348e6ff98c0fd9fa
Ryan Mattsson <ryan_site@hotmail.com>
2020-04-06 18:07:27 -0400

    ok

--- source/QuarantineConvo/Controllers/MessagingController.cs
+++ source/QuarantineConvo/Controllers/MessagingController.cs
@@ -4,17 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using QuarantineConvo.Data;
+using QuarantineConvo.Models;
 
 namespace QuarantineConvo.Controllers {
     public class MessagingController : Controller {
 
         QuarantineConvoContext db;
 
-        public MessagingController (QuarantineConvoContext _db) {
+        public MessagingController (QuarantineConvoContext _db) 
+        {
             db = _db;
         }
 
-        public IActionResult Index() {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        public IActionResult Connections()
+        {
+            Connection c = db.Connection.FirstOrDefault();
+            string tempUser = "smeath";
             return View();
         }
     }
