d16182109ca1a363937ba604e5cf8b219a2a5ba7
PassionatePassionFruit <PassionatePassionFruit@users.noreply.github.com>
2020-04-06 16:03:36 -0400

    Find Connection Created (Missing search)

--- source/QuarantineConvo/Controllers/MessagingController.cs
+++ source/QuarantineConvo/Controllers/MessagingController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuarantineConvo.Data;
+using QuarantineConvo.Models;
 
 namespace QuarantineConvo.Controllers
 {
     public class MessagingController : Controller
     {
+
+        private readonly QuarantineConvoContext db;
+
+        public MessagingController(QuarantineConvoContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult FindConnection()
+        {
+            var interests = db.Interest.ToList();
+
+            return View(interests);
+        }
+
+        public IActionResult Search()
+        {
+            return Index();
         }
 
     }
---
+++ source/QuarantineConvo/Data/QuarantineConvoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuarantineConvo.Models;
+
+namespace QuarantineConvo.Data
+{
+    public class QuarantineConvoContext : DbContext
+    {
+        public QuarantineConvoContext (DbContextOptions<QuarantineConvoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<QuarantineConvo.Models.Interest> Interest { get; set; }
+    }
+}
